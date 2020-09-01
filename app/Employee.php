44d000ee571af71ffcd60756ef89d67450f17c5a
Odunayo Ogungbure <sogungbure@gmail.com>
2020-09-01 04:55:32 +0100

    =Add relationships

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    public function manager()
+    {
+        return $this->belongsTo('App\Employee', 'manager_id');
+    }
 }
